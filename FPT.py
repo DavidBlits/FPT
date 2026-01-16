96e403b8440d3913c8e6f099f8f0253d5d9fe7f0
DavidBlits <blitsdavid@gmail.com>
2026-01-16 11:24:21 -0500

    End

--- FPT.py
+++ FPT.py
@@ -1,57 +1,14 @@
-'''
------------------------------------------------------------------------------
-Program Name: (never put your personal name or information on the Internet)
-Program Description:
 
------------------------------------------------------------------------------
-References:
-
-(put a link to your reference here but also add a comment in the code below where you used the reference)
-
------------------------------------------------------------------------------
-
-Additional Libraries/Extensions:
-
-(put a list of required extensions so that the user knows that they need to download extra features)
-
------------------------------------------------------------------------------
-
-Known bugs:
-
-(put a list of known bugs here, if you have any)
-
-----------------------------------------------------------------------------
-
-
-Program Reflection:
-I think this project deserves a level XXXXXX because ...
-
- Level 3 Requirements Met:
-• 
-•  
-•  
-•  
-•  
-• 
-
-Features Added Beyond Level 3 Requirements:
-• 
-•  
-•  
-•  
-•  
-• 
------------------------------------------------------------------------------
-'''
-
-
+#This is the program header, this game has 2 screens that switch when the screen is cliked the menu has a title screen and has instruction on the bottem of the screen
+#When the screen is cliked the gamestate menu changes into the play menu and a green frog image is spawnd on the screen
+#There also is a forest background and there are saws wiceh spawn at random positions on the screen in tyhe game
 
 # *********SETUP**********
 import pygame
 import random
 pygame.init()
 width,height=1000,667
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width,height))# Set window size
 background = pygame.image.load('images/nfbg.jpg')
 frog = pygame.transform.scale(pygame.image.load('images/idle01.png'),(96,96))
 saw_size=(random.randint(32,50))#Saw size
@@ -59,6 +16,8 @@ saw = pygame.transform.scale(pygame.image.load('images/saw01.png'),(saw_size,saw
 title=pygame.image.load('images/title.png')
 instructions = pygame.image.load('images/instructions.png')
 instructions2 = pygame.image.load('images/instructions2.png')
+pygame.mixer.music.load('sounds/menumusic.mp3')
+pygame.mixer.music.play(-1)
 scrollspeed= 3
 clock=pygame.time.Clock()
 cords=(random.randrange(200,800),(random.randint(200,600)))#Random cooridnates for saw
@@ -70,11 +29,12 @@ text='Press <s> to start'
 running=True
 while running:
     clock.tick(60)
-    for event in pygame.event.get():
+    for event in pygame.event.get(): #exit loop
         if event.type == pygame.QUIT:
             running = False
         if game_state=='MENU':
-            text1=minecraft.render('Press <s> to start',True,(0,0,0))
+
+            text1=minecraft.render('Press <s> to start',True,(0,0,0))# Rendering the fonts in
             text2=minecraft.render('Ninjafrogs Adventure',True,(0,255,0))
             screen.blit(background,(0,0))
             screen.blit(text1,(200,400))
@@ -82,12 +42,13 @@ while running:
             screen.blit(instructions2,(500,500))
             screen.blit(text2,(100,100))
             if event.type == pygame.MOUSEBUTTONDOWN: #if mouse cliked change game states
-                game_state='PLAY'
+                game_state='PLAY' #changed game state
         if game_state == 'PLAY':
             screen.blit(background,(0,0))
             screen.blit(frog,(50,500))
             screen.blit(saw,cords)
-    pygame.display.flip()
+            pygame.mixer.music.pause()
+    pygame.display.flip() #allows the game to be seen
 
 
     # *********EVENTS**********
